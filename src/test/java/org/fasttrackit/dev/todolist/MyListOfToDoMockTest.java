13ce7322582906b79d482e18173b91f5f6bc4387
Ionel Condor <icondor@sdl.com>
2016-02-13 09:38:11 +0200

    test class

---
+++ src/test/java/org/fasttrackit/dev/todolist/MyListOfToDoMockTest.java
@@ -0,0 +1,36 @@
+package org.fasttrackit.dev.todolist;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by icondor on 13/02/16.
+ */
+public class MyListOfToDoMockTest {
+
+    @Test
+    public void testGetInstance() throws Exception {
+
+    }
+
+    @Test
+    public void testAddItem() throws Exception {
+
+    }
+
+    @Test
+    public void testDoneItem() throws Exception {
+
+    }
+
+    @Test
+    public void testPrintList() throws Exception {
+
+    }
+
+    @Test
+    public void testGetList() throws Exception {
+
+    }
+}
